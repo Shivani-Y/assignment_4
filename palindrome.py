e53227d8f02fd2e1eff3c4a3e28188ca48b70496
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-13 22:13:05 +0530

    Test and code done for checking and raising an error if the functuin is not called for the string

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,7 @@
 """
 Creating a validator that checks if a string is a palindrome
 """
-def is_palindrome():
+def is_palindrome(value_enter):
     """ Function to check if palindrome"""
+    if not isinstance(value_enter, str):
+        raise ValueError("Only string can be used in the function")
--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,5 +1,11 @@
 """
 Test module for palindrome"""
 
+import pytest
+from palindrome import is_palindrome
+
 def test_for_data_type():
     """test to ckeck if datatype is string. If not then raise a value error"""
+    with pytest.raises(ValueError):#raises an error of function called for any type but integer
+        if not isinstance(is_palindrome(234), str):
+            raise ValueError("Function only workS for strings")
