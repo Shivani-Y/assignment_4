8796a5c70b5cbe0e88868bc0304bf388461b33b9
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-10-13 21:34:37 +0530

    Created modules for the project.

---
+++ palindrome.py
@@ -0,0 +1,5 @@
+"""
+Creating a validator that checks if a string is a palindrome
+"""
+def is_palindrome():
+    """ Function to check if palindrome"""
---
+++ tests/test_palindrome.py
@@ -0,0 +1,5 @@
+"""
+Test module for palindrome"""
+
+def test_for_data_type():
+    """test to ckeck if datatype is string. If not then raise a value error"""
